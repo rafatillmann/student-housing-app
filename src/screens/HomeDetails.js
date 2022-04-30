fda1ee39acf227b73c34d2edca418ad8645a6f26
Rafaela <rafaelatillmann.rt@gmail.com>
2022-04-30 19:27:41 -0300

    fav icon

--- src/screens/HomeDetails.js
+++ src/screens/HomeDetails.js
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button, Linking} from 'react-native';
- 
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState }  from 'react';
+import { Text, View, StyleSheet, Button, Pressable } from 'react-native';
+
 export default class HomeDetailsScreen extends React.Component {
   static navigationOptions = {
     title: 'Informações',
   };
- 
+
   constructor(props) {
     super(props);
     let contact = props.navigation.getParam('home');
@@ -14,28 +15,38 @@ export default class HomeDetailsScreen extends React.Component {
       email: contact.email,
       phone: contact.phone,
       lat: contact.address.geo.lat,
-      lng: contact.address.geo.lng
+      lng: contact.address.geo.lng,
+      liked: false
     };
   }
 
   render() {
     const { navigate } = this.props.navigation;
-    const { name, email, phone, lat, lng } = this.state;
+    const { name, email, phone, lat, lng, liked } = this.state;
+
+
     return (
       <View>
         <View style={styles.container}>
           <Text style={styles.contactName}>{name}</Text>
           <Text style={styles.contactDetails}>E-mail: {email}</Text>
           <Text style={styles.contactDetails}>Telefone: {phone}</Text>
+          <Pressable onPress={() => this.setState({liked: !liked})}>
+            <MaterialCommunityIcons
+              name={liked ? "heart" : "heart-outline"}
+              size={32}
+              color={liked ? "red" : "black"}
+            />
+          </Pressable>
         </View>
         <View style={styles.button} >
-          <Button title="Voltar" onPress={() => navigate('ContactList')} color='black'/>
+          <Button title="Voltar" onPress={() => navigate('HomeList')} color='black' />
         </View>
       </View>
     );
   }
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     padding: 15,
