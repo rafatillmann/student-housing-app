95ad3e237bb5d5665456dc72720f0db72e31af22
Rafaela <rafaelatillmann.rt@gmail.com>
2022-05-05 20:00:15 -0300

    estilo homeDetails e inicio async storage

--- src/screens/HomeDetails.js
+++ src/screens/HomeDetails.js
@@ -3,6 +3,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState } from 'react';
 import { Image, Pressable, StyleSheet, Text, View, Linking } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import { useFonts } from 'expo-font';
 
 export default function HomeDetailsScreen({ route, navigation }) {
 
@@ -10,9 +11,27 @@ export default function HomeDetailsScreen({ route, navigation }) {
 
   const [liked, setLiked] = useState()
 
+  const [homes, setHomes] = useState()
+
   const handleFavorite = () => {
+
     setLiked(!liked)
-    AsyncStorage.setItem('id_item', item.id)
+
+    // AsyncStorage.getItem('homes')
+    //   .then(req => JSON.parse(req))
+    //   .then(json => setHomes(json))
+
+    // AsyncStorage.setItem('homes', JSON.stringify(storage))
+    // .then(json => console.log(homes))
+
+  }
+
+  const [loaded] = useFonts({
+    Nunito: require('../../assets/fonts/Nunito-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
   }
 
   return (
@@ -20,62 +39,68 @@ export default function HomeDetailsScreen({ route, navigation }) {
       <View>
         <Image style={styles.image} source={{ uri: item.foto }} />
       </View>
-      <View style={styles.header}>
-        <View>
-          <Text style={styles.title}> {item.nome} </Text>
-          <Text> {item.endereco} </Text>
-        </View>
-        <View>
-          <Pressable onPress={() => handleFavorite()}>
+
+      <View style={styles.icons}>
+        <View style={styles.flex_icon}>
+          <Pressable style={styles.icon} onPress={() => handleFavorite()}>
             <MaterialCommunityIcons
               name={liked ? 'heart' : 'heart-outline'}
-              size={32}
+              size={40}
               color={'#E14594'}
             />
           </Pressable>
-          <MaterialCommunityIcons
-            name={'map'}
-            size={32}
-            color={'#2B3595'}
-          />
+          <Pressable style={styles.icon}>
+            <MaterialCommunityIcons
+              name={'map'}
+              size={40}
+              color={'#2B3595'}
+            />
+          </Pressable>
+
         </View>
+      </View>
+
+
+      <View style={styles.header}>
+        <Text style={{ fontFamily: 'Nunito', fontSize: 24, fontWeight: '700', color: '#182952' }}> {item.nome} </Text>
+        <Text style={{ fontFamily: 'Nunito', fontSize: 16 }}> {item.endereco} </Text>
+        <Text style={{ fontFamily: 'Nunito', fontSize: 32, fontWeight: '700', marginTop: 5, color: '#182952' }}> R$ {item.valor} </Text>
       </View>
 
       <View style={styles.about}>
 
-        <View>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}> Descrição </Text>
-          <Text> {item.descricao} </Text>
+        <View style={styles.description}>
+          <Text style={styles.sub_title}> Descrição </Text>
+          <Text style={styles.text}> {item.descricao} </Text>
         </View>
 
-        <View>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}> Informações do alojamento </Text>
-          <Text> {item.mobiliario} </Text>
-          <Text> {item.tipo_moradia} </Text>
-          <Text> {item.tipo_alojamento} </Text>
-          <Text> {item.banheiro} </Text>
-          <Text> {item.area} </Text>
-          <Text> {item.area_comum} </Text>
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.sub_title}> Informações do alojamento </Text>
+          <Text style={styles.text}> Mobiliario: {item.mobiliario} </Text>
+          <Text style={styles.text}> Tipo de Moradia: {item.tipo_moradia} </Text>
+          <Text style={styles.text}> Tipo de alojamento: {item.tipo_alojamento} </Text>
+          <Text style={styles.text}> Banheiro: {item.banheiro} </Text>
+          <Text style={styles.text}> Área: {item.area} </Text>
+          <Text style={styles.text}> Área Comum: {item.area_comum} </Text>
         </View>
 
-        <View>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}> Contato </Text>
-          <Text> {item.telefone} </Text>
-          <Text> {item.email} </Text>
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.sub_title}> Contato </Text>
+          <Text style={styles.text}> {item.telefone} </Text>
+          <Text style={styles.text}> {item.email} </Text>
         </View>
-       
 
 
-        <Pressable onPress={() => Linking.openURL(item.video)}>
-          <MaterialCommunityIcons
-            name={'play-circle'}
-            size={32}
-            color={'#2B3595'}
-          />
-        </Pressable>
-      </View>
-      <View>
-        <Text> {item.valor} </Text>
+        <View style={{ marginTop: 20 }}>
+          <Text style={styles.sub_title}> Mais detalhes </Text>
+          <Pressable onPress={() => Linking.openURL(item.video)}>
+            <MaterialCommunityIcons
+              name={'play-circle'}
+              size={32}
+              color={'#2B3595'}
+            />
+          </Pressable>
+        </View>
       </View>
 
     </ScrollView >
@@ -92,18 +117,40 @@ const styles = StyleSheet.create({
   },
   header: {
     display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flexDirection: 'row'
+    alignItems: 'flex-start',
+    marginTop: 20,
+    marginLeft: 10
   },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold'
+  icons: {
+    position: 'absolute',
+    marginTop: 210,
+    right: 0
+  },
+  flex_icon: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  icon: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 100,
+    marginRight: 10
   },
   about: {
-    marginTop: 30,
+    marginTop: 20,
     marginHorizontal: 10,
     display: 'flex',
     alignItems: 'flex-start',
+  },
+  sub_title: {
+    fontFamily: 'Nunito',
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#182952',
+    marginBottom: 10
+  }, 
+  text: {
+    fontFamily: 'Nunito', 
+    marginBottom: 5
   }
 });
