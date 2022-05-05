e4bcdcdbff93a899db26fa9f8dbaadb58bef82fb
Rafaela <rafaelatillmann.rt@gmail.com>
2022-05-05 18:33:18 -0300

    alteracoes homedetails

--- src/screens/HomeDetails.js
+++ src/screens/HomeDetails.js
@@ -1,7 +1,8 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, Button, Pressable, Image } from 'react-native';
+import { Image, Pressable, StyleSheet, Text, View, Linking } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default function HomeDetailsScreen({ route, navigation }) {
 
@@ -15,41 +16,94 @@ export default function HomeDetailsScreen({ route, navigation }) {
   }
 
   return (
-    <View>
-      <View style={styles.container}>
-        <Pressable onPress={() => handleFavorite()}>
+    <ScrollView style={styles.container}>
+      <View>
+        <Image style={styles.image} source={{ uri: item.foto }} />
+      </View>
+      <View style={styles.header}>
+        <View>
+          <Text style={styles.title}> {item.nome} </Text>
+          <Text> {item.endereco} </Text>
+        </View>
+        <View>
+          <Pressable onPress={() => handleFavorite()}>
+            <MaterialCommunityIcons
+              name={liked ? 'heart' : 'heart-outline'}
+              size={32}
+              color={'#E14594'}
+            />
+          </Pressable>
           <MaterialCommunityIcons
-            name={liked ? 'heart' : 'heart-outline'}
+            name={'map'}
             size={32}
-            color={'#E14594'}
+            color={'#2B3595'}
+          />
+        </View>
+      </View>
+
+      <View style={styles.about}>
+
+        <View>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}> Descrição </Text>
+          <Text> {item.descricao} </Text>
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}> Informações do alojamento </Text>
+          <Text> {item.mobiliario} </Text>
+          <Text> {item.tipo_moradia} </Text>
+          <Text> {item.tipo_alojamento} </Text>
+          <Text> {item.banheiro} </Text>
+          <Text> {item.area} </Text>
+          <Text> {item.area_comum} </Text>
+        </View>
+
+        <View>
+          <Text style={{ fontSize: 16, fontWeight: 'bold' }}> Contato </Text>
+          <Text> {item.telefone} </Text>
+          <Text> {item.email} </Text>
+        </View>
+       
+
+
+        <Pressable onPress={() => Linking.openURL(item.video)}>
+          <MaterialCommunityIcons
+            name={'play-circle'}
+            size={32}
+            color={'#2B3595'}
           />
         </Pressable>
       </View>
-      <View style={styles.button} >
-        <Pressable onPress={() => navigation.navigate('HomeList')}>
-          <Text>Voltar</Text>
-        </Pressable>
+      <View>
+        <Text> {item.valor} </Text>
       </View>
-      <Image sou></Image>
-    </View>
+
+    </ScrollView >
   );
 }
 
 
 const styles = StyleSheet.create({
   container: {
-    padding: 15,
+
   },
-  contactName: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    height: 44,
+  image: {
+    aspectRatio: 1.5
   },
-  contactDetails: {
-    fontSize: 16,
-    height: 44,
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
   },
-  button: {
-    padding: 15
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  about: {
+    marginTop: 30,
+    marginHorizontal: 10,
+    display: 'flex',
+    alignItems: 'flex-start',
   }
 });
